bbd7d8b969f4292bce4638c41450567b37bc4e77
David <davherm@net.chalmers.se>
2019-12-03 08:50:32 +0100

    gui done, not sure of design though

--- Frontend/src/Layout/Translation.js
+++ Frontend/src/Layout/Translation.js
@@ -1,86 +1,102 @@
 import React from 'react';
 
 class translation extends React.Component {
-    state = {
-      query: "",
-      data: [],
-      filteredData: [],
-        visible: true
-    };
+      state = {
+          query: '',
+          data: [],
+      }
   
-    handleInputChange = event => {
-      const query = event.target.value;
-  
-      this.setState(prevState => {
-        const filteredData = prevState.data.filter(element => {
-          return element.name.toLowerCase().includes(query.toLowerCase());
-        });
-  
-        return {
-          query,
-          filteredData
-        };
-      });
-    };
-  
-    getData = () => {
-      fetch(`http://localhost:4000/restaurants`)
-        .then(response => response.json())
-        .then(data => {
-          const { query } = this.state;
-          const filteredData = data.filter(element => {
-            return element.name.toLowerCase().includes(query.toLowerCase());
-          });
-  
+      handleInputChange = () => {
           this.setState({
-            data,
-            filteredData
-          });
-        });
-    };
+              query: this.search.value
+          })
+          this.filterArray();
+      }
   
-    componentWillMount() {
-      this.getData();
-    }
+      getData = () => {
+          fetch(`http://localhost:4000/restaurants`) //query db example
+          .then(response => response.json())
+          .then(responseData => {
+              // console.log(responseData)
+              this.setState({
+                  data:responseData
+              })
+          })
+      }
   
-    render() {
-      return (
-        <div id="translation form">
-            <div className="searchForm">
-            <form>
-                <input
-                placeholder="Search for.."
-                value={this.state.query}
-                onChange={this.handleInputChange}
-                />
-            </form>
-            <div>{this.state.filteredData.map(i => <p>{i.name}</p>)}</div>
-            </div>
-            
-            <div className="newTextName">
-            <form>
-            <input
-                placeholder="enter new name.."
-                value={this.state.query}
-                onChange={this.handleInputChange}
-            />
-            </form>
-            <div>{this.state.filteredData.map(i => <p>{i.name}</p>)}</div>
-            </div>
+      filterArray = () => {
+          var searchString = this.state.query;
+          var responseData = this.state.data
+          if(searchString.length > 0){
+              // console.log(responseData[i].name);
+              responseData = responseData.filter(l => {
+                  console.log( l.name.toLowerCase().match(searchString));
+              })
+          }
+      }
+  
+      componentWillMount() {
+          this.getData();
+      }
+      render() {
+          return (
+            <div id="translation">
+              <div className="searchForm">
+                  <form>
+                    <label>Search for existing text</label>
+                    <input type="text" 
+                    id="search" 
+                    placeholder="Search for ID..." 
+                    ref={input => this.search = input} 
+                    onChange={this.handleInputChange}/>
+                  </form>
+                  <div>
+                      {
+                          this.state.data.map((i) =>
+                              <p>{i.name}</p>
+                          )
+                      }
+                  </div>
+              </div>
+              <div className="newTextForm">
+                  <form>
+                    <label>Create new Text</label>
+                    <input type="text" 
+                    id="newText" 
+                    placeholder="Enter ID.." 
+                    ref={input => this.search = input} 
+                    onChange={this.handleInputChange}/>
+                  </form>
+                  <div>
+                      {
+                          this.state.data.map((i) =>
+                              <p>{i.name}</p>
+                          )
+                      }
+                  </div>
+              </div>
+              <div className="editTextForm">
+                  <form>
+                    <label>Edit text here</label>
+                    <textarea type="text" 
+                    id="editText" 
+                    placeholder="text goes here.." 
+                    ref={input => this.search = input} 
+                    onChange={this.handleInputChange}/>
+                  </form>
+                  <div>
+                      {
+                          this.state.data.map((i) =>
+                              <p>{i.name}</p>
+                          )
+                      }
+                  </div>
+              </div>
 
-            <div className="editTextName">
-            <form>
-            <input
-                placeholder="enter new name.."
-                value={this.state.query}
-                onChange={this.handleInputChange}
-            />
-            </form>
-            <div>{this.state.filteredData.map(i => <p>{i.name}</p>)}</div>
+
             </div>
-        </div>
-      );
-    }
+          )
+      }
   }
 
   export default translation;
