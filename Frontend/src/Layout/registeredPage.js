834d920548ec00b0c8a04d3274282a463b011081
Makalunga <47451240+Makalunga@users.noreply.github.com>
2019-12-03 15:06:54 +0100

    registeredpage with links

--- Frontend/src/Layout/registeredPage.js
+++ Frontend/src/Layout/registeredPage.js
@@ -1,25 +1,54 @@
 import React from 'react';
 import '../App';
 import {Link} from 'react-router-dom';
+import logo from '../Image/CIAM-logo.png'
 
- export default class registeredPage extends React.Component  {
+
+  export default class registeredPage extends React.Component  {
     render() {
     return (
-        <section>
         
-        <h1>Customer Identity and Access Management</h1>
-        
-        <h1>You have registered</h1>
-        
-        
-            <p>Please confirm your account with the email we sent you</p>
-        
-        <Link to ='/#'>
-            <a>Go back to home page</a>
-        </Link>
-        
-        </section>
-    
+        <section >
+
+               
+
+                <header className="headerDesign">
+                    
+
+
+                    <h1 className="h1Design">Customer Identity and Access Management</h1>
+
+
+                </header>
+
+                <div className="logoDesign">
+              <Link to="/#">
+              <img src={logo} alt="logo"       />
+              </Link>
+                 </div> 
+                
+            <div className="divDesign">
+                <h2 className="messageDesign">Thank you for registering!</h2>
+                <h2 className="messageDesign2">An email with a link to activate your account has been sent to you.</h2>
+            </div>
+
+            <div className="divDesign">
+                <Link to ="/loginPage">
+                    <p className="linkDesign">click here to login</p>    
+                </Link> 
+            
+               
+            </div>
+            
+                
+
+              
+
+            </section>
+            
+            
+                
+            
         );
 
     }
